83e17fffc0bcd078feb7c8e2d5373cab1951c83e
Roger Day <notsofeo@gmail.com>
2023-07-07 20:15:08 -0400

    more files to add

---
+++ index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+    <script>
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain attribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }      
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
+    }
+  }
+};
+</script>
+<head>
+    
+<title>Pan-Ethnic Tuba: Professor Beautiful</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<script language="JavaScript" type="text/JavaScript">
+<!--
+function MM_reloadPage(init) {  //reloads the window if Nav4 resized
+  if (init==true) with (navigator) {if ((appName=="Netscape")&&(parseInt(appVersion)==4)) {
+    document.MM_pgW=innerWidth; document.MM_pgH=innerHeight; onresize=MM_reloadPage; }}
+  else if (innerWidth!=document.MM_pgW || innerHeight!=document.MM_pgH) location.reload();
+}
+MM_reloadPage(true);
+//-->
+</script>
+<style type="text/css">
+.OOF {
+	font-family: Baskerville, Palatino Linotype, Palatino, Century Schoolbook L, Times New Roman, serif;
+}
+.OOF {
+	font-family: Impact, Haettenschweiler, Franklin Gothic Bold, Arial Black, sans-serif;
+}
+</style>
+</head>
+<body bgcolor="#88B7A6">
+<!--#exec cmd="cgi-bin/entropybanner.cgi"-->
+<h1><font color="#330099">Pan-Ethnic Tuba:  Professor Beautiful</font></h1>
+      <p><img src="tuba-card-horizontal.png" width="1042" height="305" alt=""/></p>
+      <h1>THIS WEBSITE was WAY WAY <span class="OOF">WAY</span> OBSOLETE!</h1>
+      <h2>Finally being REVAMPED thanks to a kick in the pants from 
+          Peter D'O the class correspondent </h2>
+    <p>...excuse my dust.<img height="10%"  src="construction-worker.jpg" > </p>
+<hr color="blue" size="10">
+    <p>Lots of my music at <a href=https://www.youtube.com/professorbeautiful target=_blank>www.professorbeautiful.org</a>. </p>
+    <p>And here are some of them stand-alone, if you prefer: <a href=videos/video-gallery-code/2a-caption-gallery.php target=_blank>this page. </a>Choose this if you are youtube-o-phobic (if you sometimes see ads).
+    </p>
+    <p>&nbsp;</p>
+<p>I've played the tuba since 1969. <br>
+    The exact same tuba. <a href="plea-to-wesleyan-alums.html"> Wesleyan alums from that era, please click here!</a> <br>
+    <br>
+Its name is Birdbath Beautiful. It writes <a href="stories">poetry</a>.</p>
+<p>My turn, wise guy: Here's a <a href=stories/musical-bio.html>musical bio</a>.
+    <br>
+And here <a href=youtube.com/professorbeautiful> at YouTube </a>playing solo on Bach, Liszt, Monk, Nazareth, Joplin, a few romani gems,<br>
+and with many amazing others throughout the years.</p>
+<p> <a href=youtube.com/professorbeautiful></a>
+</p>
+<hr size="10" color="blue">
+    <br>
+    
+<br>
+
+<blockquote>
+<table width="100%" border="0" cellspacing="2" cellpadding="2">
+
+    <tr>     
+      <td width="65%"> <h3><font face="Georgia, Times New Roman, Times, serif"><a href="why.htm"><font face="Calibri, Calisto MT, Cambria, Cambria Math, Candara, Corbel, Creeper, Felix Titling">Professor 
+          Beautiful</font></a><font face="Calibri, Calisto MT, Cambria, Cambria Math, Candara, Corbel, Creeper, Felix Titling"> 
+          is a consortium of alter-egos (pictured below) <br>
+          &amp; the go-to guy in Pittsburgh for all subversive tuba styles 
+ <a href="http://en.wikipedia.org/wiki/Pittsburgh,_Pennsylvania#Local_dialect"> 
+'n'at</a>... </font></font></h3>
+          <div align="center">
+<font face="Georgia, Times New Roman, Times, serif">Click <a href="alert.htm">here</a> 
+to join the <em><strong><font color="#660033">TubaAlert e-mail list</font></strong></em>.</font> 
+
+        <p><font face="Georgia, Times New Roman, Times, serif">Email: 
+          me AT professorbeautiful.org . </font></p>
+        <p>&nbsp;</p>
+         
+        <a href="pictures%20from%20cell%20phone%202007-09-05/guys5%20fixed.jpg">
+		  <img src="pictures%20from%20cell%20phone%202007-09-05/guys5%20fixed.jpg" width="380" height="321" border="0"
+			   alt="Pittsburgh guys"></a>
+</div>
+        </td>
+    <td>
+            <img height = "50%" src="20030602pp_tubaPJ_350.jpg" alt="."> 
+        <br>
+        Photo by: Pam Panchak / Pittsburgh 
+            Post-Gazette (6/2/03)
+        <br>
+        The day after the mayor was bitten by a dog off-leash! You can look it up.
+        <br> A couple of scofflaws here.
+        </td>
+    </tr>
+  </table>
+  
+</blockquote>
+
+<hr size="10" color="blue">
+
+    <table width="99%" height="78" border="0" cellpadding="0" cellspacing="0">
+      <tr>
+      <td width="35%" ><h2><font face="Calibri, Calisto MT, Cambria, Cambria Math, Candara, Corbel, Creeper, Felix Titling"><a href="LindaDay/GardenOfLifeAndLoveLindaDay-%20cropped.jpg"><img src="LindaDay/GardenOfLifeAndLoveLindaDay-%20cropped.jpg" width="307" height="162" border="0"></a></font></h2></td>
+      <td width="55%"><h2><font face="Calibri, Calisto MT, Cambria, Cambria Math, Candara, Corbel, Creeper, Felix Titling"><strong>My 
+          sister <a href="LindaDay/index.html">Linda Day</a> passed away just 
+          before the Iraq war began. <br>
+          She was my hero, a great healer, and best 
+          friend to many people.<br>
+          You can get to know her a bit <a href="LindaDay/index.html">even now.</a></strong></font>
+          </h2></td>
+  </tr>
+
+</table>
+
+    <hr size="10" color="blue">
+    <a id='videos'> <h2>VIDEOS</h2></a>
+    
+<?php
+    include "videos/video-gallery-code/2a-caption-gallery.php";
+?>    
+
+</body>
+
+</html>
---
+++ plea-to-wesleyan-alums.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+    Dear Wesleyanites
+    <br>
+    <pre>
+    If you know of a clarinettist present at Wesleyan in 1969
+    who headed a dixieland/ragtime band,
+    please tell me who it was.
+    He made a crazy leap of faith (or poor judgment, if that's different).
+    He invited me to a rehearsal despite at least 4 good reasons not to,
+    and only one on the other side:  my passion to return to playing music.
+    
+    This decision brought the tuba into my life 
+    and enriched it in indescribable ways.
+    On my late-life quest to thank people who have helped through the decades,
+    it would be so satisfying to give this person the deepest expression of gratitude.
+    
+    I only know that Charlie Ziff was in the band, and I think Lee Henderson.
+    Further, to play a party at an English professor's house,
+    we carried an upright piano over a chasm across a wooden bridge with twisty stairs in the middle.
+    That was December 1969.
+    </pre>
+    
+</body>
+</html>
